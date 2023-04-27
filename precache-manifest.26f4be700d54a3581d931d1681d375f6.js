076c8e18b1dfdc582d100859b294f0d346b643a5
Code Sandbox <hello@codesandbox.io>
2023-04-27 15:59:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.26f4be700d54a3581d931d1681d375f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db2b2c7866d02b9ef696379ba3c6c06",
+    "url": "https://amrmabdelazeem.github.io/csb-lv1dxi/index.html"
+  },
+  {
+    "revision": "465b23b03010b3410c59",
+    "url": "https://amrmabdelazeem.github.io/csb-lv1dxi/static/js/2.8dfa08cb.chunk.js"
+  },
+  {
+    "revision": "1f643a540e705869496d",
+    "url": "https://amrmabdelazeem.github.io/csb-lv1dxi/static/js/main.da4d482b.chunk.js"
+  },
+  {
+    "revision": "c49e7de01134f02d4259",
+    "url": "https://amrmabdelazeem.github.io/csb-lv1dxi/static/js/runtime-main.ec53cbc9.js"
+  }
+]);
